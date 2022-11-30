28bd921da20b99f4fc395f5fcdeabf687a26835b
Edson Amarante <edsonamarante@uniplaclages.edu.br>
2022-11-30 18:51:44 -0300

    acaboooooouuuuo

---
+++ mod_login/login.py
@@ -0,0 +1,113 @@
+from flask import Blueprint, render_template, request, redirect, url_for,session
+from funcoes import Funcoes
+from functools import wraps
+import requests
+
+bp_login = Blueprint('login', __name__, url_prefix='/', template_folder='templates')
+
+headers = {'x-token': 'abcBolinhasToken', 'x-key': 'abcBolinhasKey'}
+url_login = "http://localhost:8000/teste/"
+
+@bp_login.route("/", methods=['GET', 'POST'])
+def login():
+    return render_template("formLogin.html")
+
+# @bp_login.route('/login', methods=['POST'])
+# def validaLogin():
+#     try:
+#     # dados enviados via FORM
+#         cpf = request.form['usuario']
+#         senha = Funcoes.cifraSenha(request.form['senha'])
+        
+#         # limpa a sessão
+#         session.clear()
+
+#         if (cpf == "Edson" and senha == Funcoes.cifraSenha('Bolinhas')):
+#             session['login']=cpf
+
+#              # abre a aplicação na tela home
+#             return redirect(url_for('index.formListaIndex'))
+#         else:
+#             raise Exception("Falha de Login! Verifique seus dados e tente novamente!")
+
+#     except Exception as e:
+#         # retorna para a tela de login
+#         return redirect(url_for('login.login', msgErro=e.args[0]))
+
+
+
+@bp_login.route('/login', methods=['POST'])
+def validaLogin():
+    try:
+        cpf = request.form['usuario']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        payload = {
+            'cpf': cpf,
+            'senha': senha
+        }
+
+        session.clear()
+
+        autenticado = requests.post(
+            url_login, headers=headers, params=payload)
+
+        res = autenticado.json()
+        print(res)
+
+        if (res[0] is 1):
+            usuario = res[1]
+            session['login'] = usuario['nome']
+            usuario = usuario['grupo']
+            if (usuario == 1):
+                usuario = 'Atendente'
+            elif (usuario == 2):
+                usuario = 'Caixa'
+            elif (usuario == 3):
+                usuario = 'Administrador'
+
+            print(usuario)
+            session['usuario'] = usuario
+            return redirect(url_for('index.formListaIndex'))
+        else:
+            raise Exception(
+                "Falha de Login! Verifique seus dados e tente novamente!")
+    except Exception as e:
+        return redirect(url_for('login.login', msgErro=e.args[0]))
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+@bp_login.route("/logoff", methods=['GET'])
+def logoff():
+    # limpa um valor individual
+    session.pop('login', None)
+    # limpa toda sessão
+    session.clear()
+    # retorna para a tela de login
+    return redirect(url_for('login.login'))
+
+
+
+# valida se o usuário esta ativo na sessão
+def validaSessao(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'login' not in session:
+        # descarta os dados copiados da função original e retorna a tela de login
+            return redirect(url_for('login.login', msgErro='Usuário não logado!'))
+        else:
+        # retorna os dados copiados da função original
+            return f(*args, **kwargs)
+        # retorna o resultado do if acima
+    return decorated_function
--- mod_produto/produto.py
+++ mod_produto/produto.py
@@ -1,5 +1,5 @@
 from webbrowser import get
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template,request, redirect, url_for, jsonify
 import requests
 import base64
 
@@ -54,3 +54,70 @@ def insert():
 @bp_produto.route('/form-produto/', methods=['GET','POST'])
 def formProduto():
     return render_template('formProduto.html')
+
+
+
+@bp_produto.route("/form-edit-produto", methods=['POST'])
+def formEditProduto():
+    try:
+        # ID enviado via FORM
+        id_produto = request.form['id']
+        # executa o verbo GET da API buscando somente o funcionário selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(urlApiProduto + id_produto, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        # renderiza o form passando os dados retornados
+        return render_template('formProduto.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])
+
+
+
+
+
+
+
+@bp_produto.route('/edit', methods=['POST'])
+def edit():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form['id']
+        nome = request.form['nome']
+        preco = request.form['preco']
+        qtd = request.form['qtd']
+        descricao= request.form['descricao']
+        foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
+        
+
+        # monta o JSON para envio a API
+        payload = {'id':id_produto,'nome': nome, 'preco': preco, 'qtd': qtd,'descricao': descricao,  'foto': foto}
+
+        # executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(urlApiProduto + id_produto, headers=headers, json=payload)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        return redirect( url_for('produto.formListaProduto', msg=result[0]) )
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])
+
+
+@bp_produto.route('/delete', methods=['POST'])
+def delete():
+    try:
+        # dados enviados via FORM
+        id_funcionario = request.form['id_funcionario']
+        # executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(urlApiProduto + id_funcionario, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        #return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+        #print(result[0])
+        #{'msg': 'Excluído com sucesso!', 'id': 4}
+        return jsonify(erro=False, msg=result[0])
+    except Exception as e:
+        #return render_template('formListaFuncionario.html', msgErro=e.args[0])
+        return jsonify(erro=True, msgErro=e.args[0])
